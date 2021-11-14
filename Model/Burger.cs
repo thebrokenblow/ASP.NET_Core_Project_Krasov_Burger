f8a0f5aa8a38d7994205f4b1aee932b5c31210cd
Artem Krasov <artkrasov@gmail.com>
2021-11-15 01:53:49 +0300

    Added work with databases

---
+++ Model/Burger.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace KrasovBurger.Model
+{
+    public class Burger
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Burger>(this);
+    }
+}
