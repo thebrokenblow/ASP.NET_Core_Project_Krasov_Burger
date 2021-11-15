8ccca3782545b92f8ed5a7a3bfcb1c543b5daf3d
Artem Krasov <artkrasov@gmail.com>
2021-11-15 19:48:25 +0300

    Added interaction with the database

---
+++ Services/JsonFileBurgerService.cs
@@ -0,0 +1,33 @@
+﻿using KrasovBurger.Model;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Krasov_Burger.Services
+{
+    public class JsonFileBurgerService
+    {
+        public JsonFileBurgerService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "burger.json"); }
+        }
+
+        public IEnumerable<Burger> GetBurger()
+        {
+            using var jsonFileReader = File.OpenText(JsonFileName);
+            return JsonSerializer.Deserialize<Burger[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+    }
+}
